fb6cf898a85f3605ac627ed0c6a85d07d49e3982
hakan-y <hemre2620@gmail.com>
2022-01-18 20:06:47 +0300

    Server added

---
+++ server/routes/auth.js
@@ -0,0 +1,13 @@
+/** 
+    *!  Gelen requestleri gerekli parametrelere yönlendirmek.
+    */
+const express = require('express');
+
+const { signup, login } = require('../controllers/auth.js');
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+
+module.exports = router;
